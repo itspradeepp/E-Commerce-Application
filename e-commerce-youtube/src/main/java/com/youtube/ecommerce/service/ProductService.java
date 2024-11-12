2852e906e880e70fa1e2d025b50b0e97e1ad37ab
itspradeepp <91178113+itspradeepp@users.noreply.github.com>
2024-11-12 11:45:45 -0600

    Add files via upload

---
+++ e-commerce-youtube/src/main/java/com/youtube/ecommerce/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.youtube.ecommerce.service;
+
+
+import com.youtube.ecommerce.dao.ProductDao;
+import com.youtube.ecommerce.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+
+    @Autowired
+    private ProductDao productDao;
+    public Product addNewProduct(Product product){
+        return productDao.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return (List<Product>) productDao.findAll();
+    }
+}
